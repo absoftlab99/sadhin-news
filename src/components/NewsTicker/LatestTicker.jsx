0c44cc925396e43146eee964cc37061411cd30cc
absoftlab99 <abdullahtech2@outlook.com>
2024-08-20 11:50:29 +0600

    news ticker problem solved

--- src/components/NewsTicker/LatestTicker.jsx
+++ src/components/NewsTicker/LatestTicker.jsx
@@ -1,26 +1,32 @@
 'use client'
 import React, { useEffect, useState } from 'react';
 import LatestMarquee from './LatestMarquee';
+import Marquee from 'react-fast-marquee';
 
 const LatestTicker = () => {
     const [updates, setUpdates] = useState([])
 
-    useEffect(()=>{
+    useEffect(() => {
         fetch("news.json")
-        .then(res => res.json())
-        .then(data => setUpdates(data))
-    },[])
+            .then(res => res.json())
+            .then(data => setUpdates(data))
+    }, [])
     return (
-        <div className='h-[50px] grid place-content-center bg-neutral relative'>
-                <marquee>
+        <div className='flex gap-0'>
+            <div className="text-[22px] bg-error text-white font-bold h-[50px] w-[180px] px-5 grid place-content-center">
+                <p className='animate-pulse'>সর্বশেষ সংবাদ</p>
+            </div>
+            <div className='h-[50px] grid place-content-center bg-neutral w-full'>
+                <Marquee
+                pauseOnHover={true}
+                speed={100}
+                >
                     <div className='flex gap-3'>
                         {
                             updates.map(update => <LatestMarquee key={update.article_id} update={update}></LatestMarquee>)
                         }
                     </div>
-                </marquee>
-            <div className="absolute text-[22px] bg-error text-white font-bold h-[50px] left-0 top-0 px-5 grid place-content-center">
-                <p className='animate-pulse'>সর্বশেষ সংবাদ</p>
+                </Marquee>
             </div>
         </div>
     );
