e856f5b748e3f3a162da304043325176c63c5924
absoftlab99 <abdullahtech2@outlook.com>
2024-08-20 20:49:04 +0600

    add link in news ticker items

--- src/components/NewsTicker/LatestMarquee.jsx
+++ src/components/NewsTicker/LatestMarquee.jsx
@@ -1,13 +1,16 @@
 import { IconCircleFilled } from '@tabler/icons-react';
+import Link from 'next/link';
 import React from 'react';
 
 const LatestMarquee = ({update}) => {
+    // Replace spaces with hyphens, but leave other characters (like Bengali) intact
+    const cleanUrl = news.title.replace(/\s+/g, '-');
     const {title} = update
     return (
-            <div className='flex gap-2 items-center'>
+            <Link href={`/${news.category}/${news.article_id}/${cleanUrl}`} className='flex gap-2 items-center cursor-pointer'>
                 <p className='text-error animate-pulse'><IconCircleFilled></IconCircleFilled></p>
                 <h2 className='text-white text-[24px] pe-2'>{title}</h2>
-            </div>
+            </Link>
     );
 };
 
