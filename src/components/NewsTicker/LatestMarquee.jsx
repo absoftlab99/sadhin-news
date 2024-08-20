2c01e6680a12de2039fcd9440b91171bbf9092c3
absoftlab99 <abdullahtech2@outlook.com>
2024-08-20 20:52:11 +0600

    link problem solved

--- src/components/NewsTicker/LatestMarquee.jsx
+++ src/components/NewsTicker/LatestMarquee.jsx
@@ -4,13 +4,15 @@ import React from 'react';
 
 const LatestMarquee = ({update}) => {
     // Replace spaces with hyphens, but leave other characters (like Bengali) intact
-    const cleanUrl = news.title.replace(/\s+/g, '-');
-    const {title} = update
+    const cleanUrl = update.title.replace(/\s+/g, '-');
+    const {title, category, article_id} = update
     return (
-            <Link href={`/${news.category}/${news.article_id}/${cleanUrl}`} className='flex gap-2 items-center cursor-pointer'>
-                <p className='text-error animate-pulse'><IconCircleFilled></IconCircleFilled></p>
-                <h2 className='text-white text-[24px] pe-2'>{title}</h2>
-            </Link>
+            <div className=''>
+                <Link href={`/${category}/${article_id}/${cleanUrl}`} className='flex gap-2 items-center cursor-pointer'>
+                    <p className='text-error animate-pulse'><IconCircleFilled></IconCircleFilled></p>
+                    <h2 className='text-white text-[24px] pe-2'>{title}</h2>
+                </Link>
+            </div>
     );
 };
 
